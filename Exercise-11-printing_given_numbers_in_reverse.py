1517118f31fe5cffea00a970b1aaa1e3a5fb4a8e
unknown <miggy.payumo26@gmail.com>
2024-01-26 23:31:52 +0800

    user input number added

--- Exercise-11-printing_given_numbers_in_reverse.py
+++ Exercise-11-printing_given_numbers_in_reverse.py
@@ -3,3 +3,9 @@
 # 2. Evaluate the number
 # 3. Print the number in reverse order
 
+given_number = input("Please give us a number: ")
+if given_number.isdigit():
+    given_number = int(given_number)
+else:
+    print("Invalid, input must be numbers.")
+
